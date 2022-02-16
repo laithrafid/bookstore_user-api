02c41a5ee00e8eb936d640ff58293a0931232560
Laith <laith@bayt.cloud>
2022-02-16 13:42:16 -0500

    Added error package

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,28 +1,26 @@
 package users
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	Users "github.com/laithrafid/bookstore_user-api/domain/users"
 	"github.com/laithrafid/bookstore_user-api/services"
+	"github.com/laithrafid/bookstore_user-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
 	var user Users.User
-
 	if err := c.ShouldBindJSON(&user); err != nil {
-		fmt.Println(err)
-		//TODO : Return Bad request to the caller
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 	result, saveerr := services.CreateUser(user)
 	if saveerr != nil {
-		//TODO: Handle User Creation Error
+		c.JSON(saveerr.Status, saveerr)
 		return
 	}
-
 	c.JSON(http.StatusCreated, result)
 }
 
--- services/users_service.go
+++ services/users_service.go
@@ -1,7 +1,10 @@
 package services
 
-import Users "github.com/laithrafid/bookstore_user-api/domain/users"
+import (
+	Users "github.com/laithrafid/bookstore_user-api/domain/users"
+	"github.com/laithrafid/bookstore_user-api/utils/errors"
+)
 
-func CreateUser(user Users.User) (*Users.User, error) {
+func CreateUser(user Users.User) (*Users.User, *errors.RestErr) {
 	return &user, nil
 }
