afa8a04d785770cbfe3581464b0d78225a7d59ca
Laith <laith@bayt.cloud>
2022-02-27 17:04:51 -0500

    Dockerstack automation for backend

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -8,12 +8,12 @@ import (
 func mapUrls() {
 	router.GET("/ping", ping.Ping)
 
-	router.POST("/users", users.Create)
 	router.GET("/users/:user_id", users.Get)
 	router.PUT("/users/:user_id", users.Update)
 	router.PATCH("/users/:user_id", users.Update)
 	router.DELETE("/users/:user_id", users.Delete)
 	router.GET("/internal/users/search", users.Search)
 	// internal access only
+	router.POST("/users", users.Create)
 	router.POST("/users/login", users.Login)
 }
